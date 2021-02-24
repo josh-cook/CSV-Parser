12a59e28b6364c740330aa526534c9d1054a2f57
“josh-cook” <cookiej91@gmail.com>
2021-02-24 21:50:27 +0000

    Add database writer with connectors

--- src/DatabaseWriter.php
+++ src/DatabaseWriter.php
@@ -1,7 +1,33 @@
 <?php
 
 
-class DatabaseWriter
+final class DatabaseWriter
 {
 
+    const HOSTNAME = "127.0.0.1";
+    const USERNAME = "root";
+    const PASSWORD = "Password";
+    const DB = "wrenTest";
+
+    /**
+     * @param $rows
+     */
+    public static function writeToDatabase($rows) {
+        $mysqli = new mysqli(self::HOSTNAME,self::USERNAME, self::PASSWORD,self::DB);
+
+        // Check connection
+        if ($mysqli -> connect_errno) {
+            echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+            exit();
+        }
+
+        $query = $mysqli->prepare("INSERT INTO tblProductData (strProductCode, strProductName, strProductDesc, intStock, dblCost, dtmDiscontinued, dtmAdded) VALUES (?, ?, ?, ?, ?, ?, ?)");
+
+        foreach ($rows as $row) {
+            $query->bind_param("sssidss", $row[0], $row[1], $row[2], $row[3], $row[4], $row[5], $row[6]);
+            $query->execute();
+        }
+
+        $mysqli->close();
+    }
 }
