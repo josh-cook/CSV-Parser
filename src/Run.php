49be5228afcad6241dbe49535e73d554127bdb4d
“josh-cook” <cookiej91@gmail.com>
2021-02-24 21:52:00 +0000

    Add CLI runner

--- src/Run.php
+++ src/Run.php
@@ -1,1 +1,28 @@
 <?php
+
+require_once "CsvReader.php";
+require_once "DatabaseWriter.php";
+
+if ($argc < 2) {
+    echo "Error: No file submitted \n";
+    exit;
+}
+
+$file = CsvReader::fromFile($argv[1]);
+
+$invalidLines = $file["invalid"];
+$validLines = $file["lines"];
+
+echo "These records are invalid\n";
+foreach ($invalidLines as $line) {
+    echo implode(',', $line) . "\n";
+}
+
+if ($argc > 2 && $argv[2] === "test") {
+    echo "These records are valid\n";
+    foreach ($validLines as $line) {
+        echo implode(',', $line) . "\n";
+    }
+} else {
+    DatabaseWriter::writeToDatabase($validLines);
+}
